dfd1da88655e09961de48c495ab73e71afe460e2
PedroHRFerreira <devpedrohr@gmail.com>
2025-04-02 21:43:04 -0300

    fix: :bug: 75% Conectado

--- src/store/useFetchSales.ts
+++ src/store/useFetchSales.ts
@@ -4,6 +4,12 @@ export interface SaleMonth {
   label: string;
   value: number;
   product: string;
+  name?: string;
+  month?: string;
+  quantity: number;
+  price: number;
+  id_user: number;
+  id: number;
 }
 
 export interface SalesData {
@@ -13,4 +19,27 @@ export interface SalesData {
 export function useFetchSales() {
   const url = "http://localhost:8000/api/sales";
   return useFetch<SalesData>(url);
+}
+
+export async function usePostSales(newSales: SaleMonth) {
+  const response = await fetch("http://localhost:8000/api/sales", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newSales),
+  });
+
+  return response.json();
+}
+
+export async function useDeleteSales(id: number) {
+  const response = await fetch(`http://localhost:8000/api/sales/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return response.json();
 }
