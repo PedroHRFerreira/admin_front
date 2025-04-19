3fc58d3be0176aa4115f744f65ab69596376cc2a
PedroHRFerreira <devpedrohr@gmail.com>
2025-04-19 20:09:28 -0300

    fix: :fire: ...

--- src/store/useFetchGoals.ts
+++ src/store/useFetchGoals.ts
@@ -5,26 +5,3 @@ export function useFetchGoals() {
   const url = "http://localhost/api/goals";
   return useFetch<IGoals>(url);
 }
-
-// export async function usePostSales(newSales: ISales) {
-//   const response = await fetch("http://localhost/api/sales", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(newSales),
-//   });
-
-//   return response.json();
-// }
-
-// export async function useDeleteSales(id: number) {
-//   const response = await fetch(`http://localhost/api/sales/${id}`, {
-//     method: "DELETE",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   });
-
-//   return response.json();
-// }
